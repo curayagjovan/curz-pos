34606816b779c7c902f97b0cf26d2bffa9a78c29
jovan <jovan.curayag@gmail.com>
2026-07-14 17:18:07 +0800

    fix

--- app/layouts/mobile-page-wrapper.tsx
+++ app/layouts/mobile-page-wrapper.tsx
@@ -219,6 +219,10 @@ export default function MobilePageWrapper({
           borderTop: "1px solid",
           borderColor: "divider",
           borderRadius: 0,
+          backdropFilter: "saturate(160%) blur(10px)",
+          WebkitBackdropFilter: "saturate(160%) blur(10px)",
+          backgroundColor:
+            "rgba(var(--mui-palette-background-paperChannel) / 0.88)",
         }}
       >
         <BottomNavigation
